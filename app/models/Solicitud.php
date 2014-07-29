8b90724d90d568186ed4e793d2acd63dcc68ef91
henryjason <henryjason2009@hotmail.com>
2014-07-29 10:48:15 -0600

    Subir  vendor

--- app/models/Solicitud.php
+++ app/models/Solicitud.php
@@ -20,24 +20,3 @@ class Solicitud extends Eloquent
 
 
 
-create table menciones(
-	id serial not null primary key,
-	mensaje_id integer not null,
-	usuario_id integer not null,
-	created_at date NOT NULL default CURRENT_DATE,
-	estado integer not null,
-	constraint fk_menciones_usuario foreign key (usuario_id) references users(id),
-	constraint fk_menciones_mensaje foreign key (mensaje_id) references hums(id)
-	
-);
-
-create table hashtag(
-	id serial not null primary key,
-	mensaje_id integer not null,
-	hashtag varchar not null,
-	created_at date NOT NULL default CURRENT_DATE,
-	constraint fk_hashtag_mensaje foreign key (mensaje_id) references hums(id)
-	
-);
-
-
